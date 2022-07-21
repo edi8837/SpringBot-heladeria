a83b747d02c321742f76c955631bf1fa0748e050
edi8837 <adri2345salinas@gmail.com>
2022-07-21 11:42:29 -0500

    subido

---
+++ src/main/java/ec/ups/edu/Heladeria/HeladeriaApplication.java
@@ -0,0 +1,17 @@
+package ec.ups.edu.Heladeria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeladeriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeladeriaApplication.class, args);
+	}
+
+}
+
+
+
+
