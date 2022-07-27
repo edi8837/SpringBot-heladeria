7b06ea6536bf4ce0287639d0d959d359e927c829
edi8837 <adri2345salinas@gmail.com>
2022-07-27 16:34:29 -0500

    subir1

--- src/main/java/ec/ups/edu/Heladeria/servicios/UsuarioNoEncontradoException.java
+++ src/main/java/ec/ups/edu/Heladeria/servicios/UsuarioNoEncontradoException.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "Usuario no encontrado!")
 public class UsuarioNoEncontradoException extends RuntimeException{
+//hola
 
     public UsuarioNoEncontradoException(){
     }
