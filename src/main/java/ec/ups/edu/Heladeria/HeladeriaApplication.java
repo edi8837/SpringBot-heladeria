e31f35e3abd1516887c5d02f80b4b86509822bcd
edi8837 <adri2345salinas@gmail.com>
2022-07-28 02:54:12 -0500

    final

--- src/main/java/ec/ups/edu/Heladeria/HeladeriaApplication.java
+++ src/main/java/ec/ups/edu/Heladeria/HeladeriaApplication.java
@@ -1,22 +1,22 @@
-package ec.ups.edu.Heladeria;
+
+
+        package ec.ups.edu.Heladeria;
 
 import org.springframework.boot.SpringApplication;
-        import org.springframework.boot.autoconfigure.SpringBootApplication;
-        import org.springframework.boot.builder.SpringApplicationBuilder;
-        import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HeladeriaApplication extends SpringBootServletInitializer {
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder){
-        return builder.sources(HeladeriaApplication.class);
-    }
+public class HeladeriaApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(HeladeriaApplication.class, args);
     }
 
 }
+
+
+
+
+
 
 
